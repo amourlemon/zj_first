84b58bd891b7533feb77a849b457feb1afe201a7
zhangjian199705 <953593486@qq.com>
2021-12-02 14:07:08 +0800

    测试Json.stringfy(obj)的类型

--- js/code/17_ES6其他知识点/07_展开语法进行的浅拷贝.js
+++ js/code/17_ES6其他知识点/07_展开语法进行的浅拷贝.js
@@ -4,10 +4,12 @@ const info = {
     friend: {name: '李四'}
 }
 
-const obj = {...info, address: '上海'};
+let obj = {...info, address: '上海'};
 
 obj.name = '王五';
 obj.friend.name = '赵六';
 
 console.log(obj);
 console.log(info);
+obj = JSON.stringify(obj)
+console.log(typeof obj === 'string');
