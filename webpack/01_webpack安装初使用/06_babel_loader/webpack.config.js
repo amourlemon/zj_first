0708b192a638043fe7efeb547d8ea8a43b837036
zhangjian199705 <953593486@qq.com>
2022-04-28 21:13:59 +0800

    webpack loader的学习

--- webpack/01_webpack安装初使用/06_babel_loader/webpack.config.js
+++ webpack/01_webpack安装初使用/06_babel_loader/webpack.config.js
@@ -113,6 +113,21 @@ module.exports = {
         parser: {
           parse: yaml.parse
         }
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: [
+              ['@babel/plugin-transform-runtime', {
+                "regenerator": true
+              }]
+            ]
+          }
+        }
       }
     ]
   },
